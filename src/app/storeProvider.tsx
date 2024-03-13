144ff42a04e93ddbf7db5484c3f84d732aa832e6
Gerson Soto <gersonsoto@habi.co>
2024-03-13 10:09:51 -0500

    [Added] Initial configurations (Jest, Redux, Sass)

---
+++ src/app/storeProvider.tsx
@@ -0,0 +1,18 @@
+'use client';
+import { AppStore, makeStore } from '@/redux/store/store';
+import { useRef } from 'react';
+import { Provider } from 'react-redux';
+
+export default function StoreProvider({
+	children,
+}: Readonly<{
+	count: number;
+	children: React.ReactNode;
+}>) {
+	const storeRef = useRef<AppStore | null>(null);
+	if (!storeRef.current) {
+		storeRef.current = makeStore();
+	}
+
+	return <Provider store={storeRef.current}>{children}</Provider>;
+}
---
+++ src/hooks/useRedux.ts
@@ -0,0 +1,11 @@
+/**
+ * Custom hooks for Redux integration in React components.
+ */
+
+import { AppDispatch, AppStore, RootState } from '@/redux/store/store';
+import { useDispatch, useSelector, useStore } from 'react-redux';
+import type { TypedUseSelectorHook } from 'react-redux';
+
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+export const useAppStore: () => AppStore = useStore;
