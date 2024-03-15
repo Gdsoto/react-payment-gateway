d547f759ae7d63d7745fd456d39061460f49d267
Gerson Soto <gersonsoto@habi.co>
2024-03-15 16:50:11 -0500

    [Added] Jest configs

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
 const path = require('path');
 
 module.exports = {
-	sassOptions: {
-		includePaths: [path.join(__dirname, 'styles')],
-	},
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'fakestoreapi.com',
+        port: '',
+        pathname: '/img/**',
+      },
+    ],
+  },
 };
