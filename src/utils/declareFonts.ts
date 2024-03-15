1ca403a51c38a1dab0aca84a9f33a562d81bc63b
Gerson Soto <gersonsoto@habi.co>
2024-03-15 09:26:18 -0500

    [Added] font configs & sass variables

---
+++ src/utils/declareFonts.ts
@@ -0,0 +1,9 @@
+import { Inter } from 'next/font/google';
+
+export const inter = Inter({
+	subsets: ['latin'],
+	style: 'normal',
+	variable: '--font-inter',
+	display: 'swap',
+	weight: ['300', '400', '500', '600', '700', '800'],
+});
