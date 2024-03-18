23c545d36f0bf3ec0d4238068e00e549601a4a30
Gerson Soto <gersonsoto@habi.co>
2024-03-17 19:39:50 -0500

    [Adjust] layout structure

--- src/components/layouts/BaseLayout.tsx
+++ src/components/layouts/BaseLayout.tsx
@@ -3,13 +3,21 @@ import React from 'react';
 // Styles
 import styles from './BaseLayout.module.scss';
 import StoreProvider from '@/app/storeProvider';
+import NavBar from '../NavBar/NavBar';
+
+// Libs
+import { Toaster } from 'react-hot-toast';
 
 const BaseLayout = ({ children }: { children: React.ReactNode }) => {
-	return (
-		<StoreProvider>
-			<main className={styles.baseContainer}>{children}</main>
-		</StoreProvider>
-	);
+  return (
+    <StoreProvider>
+      <Toaster position='top-center' reverseOrder={false} />
+      <main className={styles.baseContainer}>
+        <NavBar />
+        {children}
+      </main>
+    </StoreProvider>
+  );
 };
 
 export default BaseLayout;
