d33e6d91ad829741cb776a05b316ac6d8a24398d
Gerson Soto <gersonsoto@habi.co>
2024-03-17 19:38:55 -0500

    [Added] react libs && new assets

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,9 @@ import React from 'react';
 
 // Components
 import CartResume from '@/components/CartResume/CartResume';
-import NavBar from '@/components/NavBar/NavBar';
 
 const Home = () => {
-	return (
-		<>
-			<NavBar />
-			<CartResume />
-		</>
-	);
+  return <CartResume />;
 };
 
 export default Home;
